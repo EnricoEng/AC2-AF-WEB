287c3732bc83b52045b743018db1fef245a543b4
leonardo.bottaro <leonardooloiro@hotmail.com>
2021-10-23 17:04:56 -0300

    COmeço do SOCKETIO

--- public/server.js
+++ public/server.js
@@ -2,13 +2,27 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static('public'))
 
-app.get('/index', function(request, response){
+app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    })
+})
+
+io.on('sendMessage', (socket) => {
+    socket.on('sendMessage', ()=>{
+        console.log('funciona desgraça')
+    })
+})
 
 server.listen(3000, ()=> {
     console.log("Server is up")
