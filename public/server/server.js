516b387daaa9004ad92298782bb0be8d41332ebf
leonardo.bottaro <leonardooloiro@hotmail.com>
2021-10-23 12:47:17 -0300

    mudanças

--- public/server/server.js
+++ public/server/server.js
@@ -1,30 +1,13 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
 
-const publicPath = path.join(__dirname, "public");
-const port = process.env.PORT || 3000;
-const app = express();
-let server = http.createServer(app);
-let io = socketIO(server);
+app.get('/', (request, response)=>{
+    console.log('hello world')
+})
 
-app.use(express.static(publicPath));
-server.listen(port, ()=> {
-    console.log(`Server is up on port ${port}.`)
-});
 
-app.get("/index.html", function(request, response) {
-    response.sendFile(__dirname + "/public/index.html");
- });
-
-io.on('connection', (socket) => {
-    console.log('A user just connected.');
-    socket.on('disconnect', () => {
-        console.log('A user has disconnected.');
-    })
-});
-
-io.on('startGame', () => {
-    io.emit('startGame');
+server.listen(3000, ()=> {
+    console.log("Server is up")
 })
