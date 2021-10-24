e0417b4b6190ca024a9dc6cd2631f05e755f3eaa
leonardo.bottaro <leonardooloiro@hotmail.com>
2021-10-24 12:18:15 -0300

    Commit Final AC2

--- public/server.js
+++ public/server.js
@@ -5,6 +5,7 @@ const server = http.createServer(app)
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
 app.use(express.static('public'))
 
 app.get('/', function(request, response){
@@ -16,19 +17,11 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
       console.log('user disconnected');
     })
-    socket.on('clientToServer',data =>{
-        console.log(data)
-    })
-    socket.on('clientToClient',data =>{
-        socket.broadcast.emit('serveToClient',data)
+    socket.on('start',() =>{
+        console.log('Game Start')
     })
 })
 
-io.on('connection', (socket) => {
-    socket.on('sendMessage', ()=>{
-        io.emit('funciona desgraça')
-    })
-})
 
 server.listen(3000, ()=> {
     console.log("Server is up")
