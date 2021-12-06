58adba17e2652f994ad1cc3eed6d00399ac06215
Enrico Carrano <28276248+EnricoEng@users.noreply.github.com>
2021-12-05 22:35:38 -0300

    nipple funcionando e atualizando players

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = http.createServer(app)
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let players_socket = new Array();
 
 app.use(express.static(__dirname + '/public'))
 console.log(__dirname)
@@ -19,8 +20,31 @@ app.get('/', function(request, response){
     response.sendFile(__dirname + '/public/index.html')
 })
 
+const listaJogadores = [];
+setInterval(()=>{
+    console.log(listaJogadores)
+}, 5000)
 io.on('connection', (socket) => {
-    console.log('a user connected', socket.id); //socket id diferentes
+    console.log(listaJogadores.includes(socket.id))
+    
+    socket.on('adicionarJogador', ()=>{
+        if(!listaJogadores.includes(socket.id)){
+            listaJogadores.push(socket.id)
+            
+            io.emit('adicionarPlayer', {id:socket.id, quantidade: listaJogadores.length})
+        }
+    })
+    socket.on('moverJogador', (posicoes)=>{
+        io.emit('atualizarPosicao', {
+            id: socket.id,
+            ...posicoes
+        })
+    })
+    console.log('a user connected', socket.id); 
+    players_socket.push(socket.id);
+    console.log(`Tamanho do array de players: ${players_socket}`);
+    io.emit('qtdPlayers', players_socket);
+    //socket id diferentes
     //socket.on('disconnect', () => {
     //  console.log('user disconnected');
     //})
@@ -28,13 +52,22 @@ io.on('connection', (socket) => {
         console.log('Game Start')
         io.emit('startedGame')
     })
-    socket.on('playerPosition',(players) =>{
-        console.log(`Position player1: ${players.player1}`)
-        console.log(`Position player2: ${players.player2}`)
-        io.emit('playersAtualizacoes', players)
+    socket.on('TuEsControlador1', ()=>{
+        console.log('chamando Controlador 1')
+        socket.emit('Controlador1')
     })
+    socket.on('Nippleplayer1Position', (position)=>{
+        console.log('Server Nippleplayer1Position')
+        socket.emit('Nippleplayer1Position', position)
+    })
+
+   // socket.on('playerPosition',(players) =>{
+    //    console.log(`Position player1: ${players.player1}`)
+   //     console.log(`Position player2: ${players.player2}`)
+   //     io.emit('playersAtualizacoes', players)
+  //  })
     socket.on('ballPosition', (bola) => {
-        console.log(`Ball position: ${bola}`)
+        //console.log(`Ball position: ${bola}`)
         io.emit('ballPosit', bola)
     })
 })
